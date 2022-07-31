104f874ac2d4f73d6232e55859525c5f7cf3a567
Neil Wang <wangweili457@gmail.com>
2022-07-31 16:32:33 +0800

    TEST: should_bind_class_to_a_default_constructor

--- src/main/java/com/epiphany/Context.java
+++ src/main/java/com/epiphany/Context.java
@@ -4,7 +4,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Context {
-    private final Map<Class<?>, Object> components = new HashMap<>();
+    private final Map<Class<?>, Object> components;
+
+    public Context() {
+        this.components = new HashMap<>();
+    }
 
     public <ComponentType> void bind(final Class<ComponentType> type, final ComponentType instance) {
         components.put(type, instance);
@@ -13,5 +17,10 @@ public class Context {
     @SuppressWarnings("unchecked")
     public <ComponentType> ComponentType get(final Class<ComponentType> type) {
         return (ComponentType) components.get(type);
+    }
+
+    public <ComponentType, ComponentImplementation extends ComponentType>
+    void bind(final Class<ComponentType> type, final Class<ComponentImplementation> implementationType) {
+
     }
 }
--- src/test/java/com/epiphany/ContainerTest.java
+++ src/test/java/com/epiphany/ContainerTest.java
@@ -3,11 +3,13 @@ package com.epiphany;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ContainerTest {
     interface Component {
+    }
 
+    static class ComponentWithDefaultConstructor implements Component {
     }
 
     @Nested
@@ -24,7 +26,14 @@ public class ContainerTest {
 
         @Nested
         public class ConstructorInjection {
-
+            @Test
+            void should_bind_class_to_a_default_constructor() {
+                Context context = new Context();
+                context.bind(Component.class, ComponentWithDefaultConstructor.class);
+                Component instance = context.get(Component.class);
+                assertNotNull(instance);
+                assertTrue(instance instanceof ComponentWithDefaultConstructor);
+            }
         }
 
         @Nested
