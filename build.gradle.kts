d74a4abd01f77e61264a420a7eb9189797778de5
Neil Wang <wangweili457@gmail.com>
2022-07-31 15:37:49 +0800

    DONE: Add jacoco check.

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,7 @@ version = "1.0.0"
 repositories {
     mavenCentral()
 }
+
 dependencies {
     implementation("jakarta.inject:jakarta.inject-api:2.0.1")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
@@ -25,4 +26,47 @@ tasks.withType<Test> {
 java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.isEnabled = true
+        html.isEnabled = true
+    }
+}
+
+tasks.jacocoTestCoverageVerification {
+    dependsOn(tasks.jacocoTestReport)
+    violationRules {
+        rule {
+            element = "PACKAGE"
+            limit {
+                counter = "INSTRUCTION"
+                value = "COVEREDRATIO"
+                minimum = BigDecimal(1.0)
+            }
+            limit {
+                counter = "LINE"
+                value = "COVEREDRATIO"
+                minimum = BigDecimal(1.0)
+            }
+            limit {
+                counter = "CLASS"
+                value = "COVEREDRATIO"
+                minimum = BigDecimal(1.0)
+            }
+            limit {
+                counter = "BRANCH"
+                value = "COVEREDRATIO"
+                minimum = BigDecimal(1.0)
+            }
+        }
+    }
+}
+
+tasks.test {
+    finalizedBy(tasks.jacocoTestCoverageVerification)
+}
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
 }
