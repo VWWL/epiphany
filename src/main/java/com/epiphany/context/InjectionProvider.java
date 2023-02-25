be202315e333eef21355bd8a810d658f2a9a866d
Neil <wangweili457@gmail.com>
2023-02-26 01:39:16 +0800

    refactor: initInjectMethods

--- src/main/java/com/epiphany/context/InjectionProvider.java
+++ src/main/java/com/epiphany/context/InjectionProvider.java
@@ -95,7 +95,7 @@ public final class InjectionProvider<Type> implements Provider<Type> {
         while (current != Object.class) {
             List<Method> currentInjectMethods = injectableStream(current.getDeclaredMethods())
                 .filter(o -> injectMethods.stream().noneMatch(m -> m.getName().equals(o.getName()) && Arrays.equals(m.getParameterTypes(), o.getParameterTypes())))
-                .filter(o -> stream(component.getDeclaredMethods()).filter(m -> !m.isAnnotationPresent(Inject.class)).noneMatch(m -> m.getName().equals(o.getName()) && Arrays.equals(m.getParameterTypes(), o.getParameterTypes())))
+                .filter(o -> injectableStream(component.getDeclaredMethods()).noneMatch(m -> m.getName().equals(o.getName()) && Arrays.equals(m.getParameterTypes(), o.getParameterTypes())))
                 .toList();
             injectMethods.addAll(currentInjectMethods);
             current = current.getSuperclass();
