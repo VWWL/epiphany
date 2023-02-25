bdbe9115f6216684efd52b34c1de9b8699eb6eeb
Neil <wangweili457@gmail.com>
2023-02-26 03:45:45 +0800

    refactor: InjectComponent

--- src/main/java/com/epiphany/context/InjectMethods.java
+++ src/main/java/com/epiphany/context/InjectMethods.java
@@ -30,28 +30,28 @@ public class InjectMethods {
     }
 
     @SuppressWarnings("all")
-    public static Object[] toDependencies(Context context, Executable executable) {
+    public Object[] toDependencies(Context context, Executable executable) {
         return stream(executable.getParameters()).map(Parameter::getType).map(context::get).map(Optional::get).toArray(Object[]::new);
     }
 
-    private static <Type> List<Method> initInjectMethods(Class<Type> component) {
-        List<Method> methods = new Traverser<Method>().traverse(component, (methods1, current) -> InjectStream.of(current.getDeclaredMethods()).injectablePart()
-            .filter(o -> isOverrideByInjectMethod(methods1, o))
+    private <Type> List<Method> initInjectMethods(Class<Type> component) {
+        List<Method> methods = new Traverser<Method>().traverse(component, (m, current) -> InjectStream.of(current.getDeclaredMethods()).injectablePart()
+            .filter(o -> isOverrideByInjectMethod(m, o))
             .filter(o -> isOverrideByNoInjectMethod(component, o))
             .toList());
         Collections.reverse(methods);
         return methods;
     }
 
-    private static <Type> boolean isOverrideByNoInjectMethod(Class<Type> component, Method method) {
+    private <Type> boolean isOverrideByNoInjectMethod(Class<Type> component, Method method) {
         return InjectStream.of(component.getDeclaredMethods()).notInjectablePart().noneMatch(m -> isOverride(method, m));
     }
 
-    private static boolean isOverrideByInjectMethod(List<Method> injectMethods, Method method) {
+    private boolean isOverrideByInjectMethod(List<Method> injectMethods, Method method) {
         return injectMethods.stream().noneMatch(m -> isOverride(method, m));
     }
 
-    private static boolean isOverride(Method first, Method another) {
+    private boolean isOverride(Method first, Method another) {
         return another.getName().equals(first.getName()) && Arrays.equals(another.getParameterTypes(), first.getParameterTypes());
     }
 
