ac5f5427407d4c71a296d23f12df910a596694aa
Neil <wangweili457@gmail.com>
2023-02-26 00:02:27 +0800

    fix: ContainerTest

--- src/test/java/com/epiphany/context/ContainerTest.java
+++ src/test/java/com/epiphany/context/ContainerTest.java
@@ -20,21 +20,39 @@ public class ContainerTest {
     @Nested
     public class ComponentConstruction {
 
-        @Test
-        void should_bind_type_to_a_specific_instance() {
-            Component instance = new Component() {};
-            config.bind(Component.class, instance);
-            assertSame(instance, config.context().get(Component.class).get());
-        }
+        @Nested
+        class Binding {
 
-        @Test
-        void should_return_empty_when_component_not_found() {
-            Optional<Component> component = config.context().get(Component.class);
-            assertEquals(Optional.empty(), component);
+            @Test
+            void should_bind_type_to_a_specific_instance() {
+                Component instance = new Component() {};
+                config.bind(Component.class, instance);
+                assertSame(instance, config.context().get(Component.class).get());
+            }
+
+            @Test
+            void should_return_empty_when_component_not_found() {
+                Optional<Component> component = config.context().get(Component.class);
+                assertEquals(Optional.empty(), component);
+            }
+
         }
 
         @Nested
-        public class DependencyCheck {
+        class DependencyCheck {
+
+            @Test
+            void should_not_throw_if_dependencies_are_all_distinct() {
+                config.bind(Component.class, ComponentWithInjectConstructor.class);
+                config.bind(Dependency.class, DependencyWithInjectConstructor.class);
+                config.bind(String.class, "");
+                assertDoesNotThrow(() -> config.context());
+            }
+
+        }
+
+        @Nested
+        class ErrorDependencyCheck {
 
             @Test
             void should_throw_when_dependency_is_not_found() {
