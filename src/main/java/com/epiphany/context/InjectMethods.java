490ede8b23052705836cb1d617aa5f6a4d2ad4f2
Neil <wangweili457@gmail.com>
2023-02-26 03:23:55 +0800

    refactor: InjectMethods

---
+++ src/main/java/com/epiphany/context/InjectMethods.java
@@ -0,0 +1,39 @@
+package com.epiphany.context;
+
+import java.lang.reflect.Method;
+import java.util.*;
+
+public class InjectMethods {
+
+    private final List<Method> injectMethods;
+
+    public <Type> InjectMethods(Class<Type> component) {
+        this.injectMethods = initInjectMethods(component);
+    }
+
+    public List<Method> get() {
+        return injectMethods;
+    }
+
+    private static <Type> List<Method> initInjectMethods(Class<Type> component) {
+        List<Method> methods = new Traverser<Method>().traverse(component, (methods1, current) -> InjectStream.of(current.getDeclaredMethods()).injectablePart()
+            .filter(o -> isOverrideByInjectMethod(methods1, o))
+            .filter(o -> isOverrideByNoInjectMethod(component, o))
+            .toList());
+        Collections.reverse(methods);
+        return methods;
+    }
+
+    private static <Type> boolean isOverrideByNoInjectMethod(Class<Type> component, Method method) {
+        return InjectStream.of(component.getDeclaredMethods()).notInjectablePart().noneMatch(m -> isOverride(method, m));
+    }
+
+    private static boolean isOverrideByInjectMethod(List<Method> injectMethods, Method method) {
+        return injectMethods.stream().noneMatch(m -> isOverride(method, m));
+    }
+
+    private static boolean isOverride(Method first, Method another) {
+        return another.getName().equals(first.getName()) && Arrays.equals(another.getParameterTypes(), first.getParameterTypes());
+    }
+
+}
--- src/main/java/com/epiphany/context/InjectionProvider.java
+++ src/main/java/com/epiphany/context/InjectionProvider.java
@@ -13,14 +13,14 @@ public final class InjectionProvider<Type> implements Provider<Type> {
 
     private final InjectConstructor<Type> constructors;
     private final InjectFields injectFields;
-    private final List<Method> injectMethods;
+    private final InjectMethods injectMethods;
 
     public InjectionProvider(final Class<Type> component) {
         checkConstructor(component);
         this.constructors = new InjectConstructor<>(component);
         this.injectFields = new InjectFields(component);
-        this.injectMethods = initInjectMethods(component);
-        if (injectMethods.stream().anyMatch(o -> o.getTypeParameters().length != 0)) throw new IllegalComponentException();
+        this.injectMethods = new InjectMethods(component);
+        if (injectMethods.get().stream().anyMatch(o -> o.getTypeParameters().length != 0)) throw new IllegalComponentException();
     }
 
     @Override
@@ -29,7 +29,7 @@ public final class InjectionProvider<Type> implements Provider<Type> {
         return evaluate(() -> {
             Type instance = constructors.newInstance(context);
             injectFields.injectInto(context, instance);
-            for (Method method : injectMethods) {
+            for (Method method : injectMethods.get()) {
                 method.setAccessible(true);
                 method.invoke(instance, toDependencies(context, method));
             }
@@ -41,7 +41,7 @@ public final class InjectionProvider<Type> implements Provider<Type> {
     public List<Class<?>> dependencies() {
         return Stream.of(
             injectFields.dependencies(),
-            injectMethods.stream().flatMap(m -> stream(m.getParameterTypes())),
+            injectMethods.get().stream().flatMap(m -> stream(m.getParameterTypes())),
             constructors.dependencies()
         ).flatMap(o -> o).collect(Collectors.toList());
     }
@@ -64,23 +64,6 @@ public final class InjectionProvider<Type> implements Provider<Type> {
         return stream(implementation.getConstructors()).filter(c -> c.isAnnotationPresent(Inject.class)).count();
     }
 
-    private static <Type> List<Method> initInjectMethods(Class<Type> component) {
-        List<Method> methods = new Traverser<Method>().traverse(component, (methods1, current) -> InjectStream.of(current.getDeclaredMethods()).injectablePart()
-            .filter(o -> isOverrideByInjectMethod(methods1, o))
-            .filter(o -> isOverrideByNoInjectMethod(component, o))
-            .toList());
-        Collections.reverse(methods);
-        return methods;
-    }
-
-    private static <Type> boolean isOverrideByNoInjectMethod(Class<Type> component, Method method) {
-        return InjectStream.of(component.getDeclaredMethods()).notInjectablePart().noneMatch(m -> isOverride(method, m));
-    }
-
-    private static boolean isOverrideByInjectMethod(List<Method> injectMethods, Method method) {
-        return injectMethods.stream().noneMatch(m -> isOverride(method, m));
-    }
-
     @SuppressWarnings("all")
     private static Object toDependency(Context context, Field field) {
         return context.get(field.getType()).get();
@@ -89,10 +72,6 @@ public final class InjectionProvider<Type> implements Provider<Type> {
     @SuppressWarnings("all")
     public static Object[] toDependencies(Context context, Executable executable) {
         return stream(executable.getParameters()).map(Parameter::getType).map(context::get).map(Optional::get).toArray(Object[]::new);
-    }
-
-    private static boolean isOverride(Method first, Method another) {
-        return another.getName().equals(first.getName()) && Arrays.equals(another.getParameterTypes(), first.getParameterTypes());
     }
 
 }
